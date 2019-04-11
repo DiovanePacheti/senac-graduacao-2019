bfefeef4710f2cec06b2b2ae7748d4d29a92c7aa
DiovanePacheti <frontendestudo@gmail.com>
2019-04-11 14:59:35 -0300

    criando a logica do arquivo aposentadoria.js

--- logicaDeProgramacao/aposentadoria/aposentadoria.js
+++ logicaDeProgramacao/aposentadoria/aposentadoria.js
@@ -6,8 +6,27 @@ function verificarAposentadoria(){
     var inIdade = document.getElementById("inIdade");
     var idade = Number(inIdade.value);
 
+    var faltaIdade;
+    var faltaContribuicao;
+    var saida;
+
+    if(idade >= 60 && contribuicao >= 30){
+
+        saida = "Pode se aposentar ! ";
+    }else{
+        faltaIdade = 60 - idade;
+	faltaContribuicao = 30 - contribuicao;
+
+	if(faltaIdade > faltaContribuicao){
+		saida = "Faltam " + faltaIdade + " anos pra aposentadoria por idade ";
+	}else{
+		saida = "Faltam " + faltaContribuicao + " anos de tempo de contribuição para aposentadoria";
+	}
+
+    }
+
     var outVerificar = document.getElementById("outVerificar");
-    outVerificar.textContent = "saida";
+    outVerificar.textContent = saida;
 
 }//fim da function
 var btVerificar = document.getElementById("btVerificar");
