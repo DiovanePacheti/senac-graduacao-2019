d24b038581f68789b7df4f24fefd2f3c890219f3
DiovanePacheti <frontendestudo@gmail.com>
2019-05-24 01:33:20 -0300

    criando diretorio do exercicio conta numeros impar e par com a estrutura do while

---
+++ logicaDeProgramacao/contanumerosparimparate0/parimparcontaDoWhile.js
@@ -0,0 +1,15 @@
+function verificaParImpar(){
+    var inNumero = document.getElementById("inNumero");
+    var outResposta = document.getElementById("outResposta");
+
+    var numero = Number(inNumero.value);
+
+    if(numero%2==0){
+        outResposta.textContent = "O numero " + numero + " é Par ! ";
+    }else{
+        outResposta.textContent = "O numero " + numero + "é Impar !";
+    }
+}
+var
+var btverificar = document.getElementById("btverificar");
+btverificar.addEventListener("click", verificaParImpar);
