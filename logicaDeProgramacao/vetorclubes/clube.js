db505f573bcdccbe340271ad9f6706c119e758d0
DiovanePacheti <frontendestudo@gmail.com>
2019-06-11 23:32:57 -0300

    deletando arquivos e editando outros

---
+++ logicaDeProgramacao/vetorclubes/clube.js
@@ -0,0 +1,12 @@
+var clubes[];
+var inClubes = document.getElementById("inClubes");
+
+function adicionar(){
+
+
+alert("oi");
+
+}//fim da função
+var btAdd = document.getElementById("btAdd");
+
+btAdd.addEventListener("click", adicionar);
