9567277f0a3db853d44c43b7499526a68d2aaa88
DiovanePacheti <frontendestudo@gmail.com>
2019-06-15 23:52:24 -0300

    arrumando os arquivos letras html/js

--- logicaDeProgramacao/quantasvezesletraaparece/letras.js
+++ logicaDeProgramacao/quantasvezesletraaparece/letras.js
@@ -1,1 +1,21 @@
-function
+function calcular() {
+  
+var inPalavra = document.getElementById("inPalavra");
+var palavra = inPalavra.value.toLowerCase();
+
+var inLetra = document.getElementById("inLetra");
+var letra = inLetra.value.toLowerCase();
+
+var outSaida = document.getElementById("outSaida");
+
+var cont = 0;
+
+for(var i = 0; i < palavra.length;i++){
+    if(palavra.charAt(i) == letra){
+        cont++;
+    }
+}
+outSaida.textContent = "A letra ( " + letra + " ) aparece " + cont + " veze(s) ";
+}
+var btconta = document.getElementById("btconta");
+btconta.addEventListener("click", calcular);
