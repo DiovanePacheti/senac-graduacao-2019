c9579ad96b5019280b4b2f0e356eb5b2762733aa
DiovanePacheti <frontendestudo@gmail.com>
2019-06-16 11:18:28 -0300

    arrumando os exercicios

--- logicaDeProgramacao/palavrainvertida/palavrainvertida.js
+++ logicaDeProgramacao/palavrainvertida/palavrainvertida.js
@@ -5,10 +5,19 @@ function convertendo() {
 
     var outSaida = document.getElementById("outSaida");
     outSaida.textContent = palavra + reverse(palavra);
-	
 
+    var palavra = inPalavra.value;
+    
+    var inverte = " ";
+
+    var inverte += palavra.charAt(0).toUpperCase();
+	
+    for(var i = palavra.lenght-1; i > 1 ; i--){
+	inverte += palavra.charAt(i).toLowerCase();
+    }
+    
+    outSaida.textContent = " : " + inverte;
 
 }
-
 var btInverter = document.getElementById("btInverter");
 btInverter.addEventListener("click", convertendo);
