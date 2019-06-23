d727f0cdc2c59d2bc50a95d385485ad184525a42
DiovanePacheti <frontendestudo@gmail.com>
2019-06-23 14:08:30 -0300

    inserindo o primeiro exercicio da avaliação do dia 21-06-2019 do mesmo modo como eu fiz em aula se nenhuma alteração exceto no html que seque o mesmo modelo dos arquivos ja salvos aqui

---
+++ logicaDeProgramacao/avaliacao21062019/restaurante/restaurante.js
@@ -0,0 +1,46 @@
+var mesas = [];
+var contas = [];
+
+var inMesa = document.getElementById("inMesa");
+var inConta = document.getElementById("inConta");
+
+function add (){
+
+    var mesa = inMesa.value;
+    var conta = Number(inConta.value);
+
+    mesas.push(mesa);
+    contas.push(conta);
+
+
+    var outSaida = document.getElementById("outSaida");
+    outSaida.textContent = mesas + " - " + contas + "\n";
+
+    
+    inConta.value = " ";
+    inMesa.value = " ";
+    inMesa.focus();
+    return;
+}//fim da funçao
+var btAdd = document.getElementById("btAdd");
+btAdd.addEventListener("click", add);
+
+function taxa (){
+    
+    var taxa = 0.10;
+
+    var mesasTaxadas = " ";
+
+    var outSaidaTaxa = document.getElementById("outSaidaTaxa");
+
+    for(var i = 0 ; i < contas.length; i++){
+        mesasTaxadas += mesas[i] + " valor da mesa com taxa de 10% = " + (contas[i] * taxa) + "\n"; 
+    }
+
+    outSaidaTaxa.textContent = mesasTaxadas;
+
+
+
+}//fim da function taxa
+var btTaxa = document.getElementById("btTaxa");
+btTaxa.addEventListener("click", taxa);
