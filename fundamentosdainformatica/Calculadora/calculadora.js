4aeb3637d5a04facd7085dd8076fc9001a56501d
DiovanePacheti <frontendestudo@gmail.com>
2019-07-01 15:48:16 -0300

    adicionanndo funcionalidades as teclas da calculadora

---
+++ fundamentosdainformatica/Calculadora/calculadora.js
@@ -0,0 +1,53 @@
+var outpromocao = document.getElementById("outpromocao");
+
+function botao0() {
+    var btZero = document.getElementById("btZero");
+    var zero = btZero.value;
+
+    outpromocao.textContent += zero;
+
+}
+
+function botao1() {
+    var btNove = document.getElementById("btUm");
+    var nove = btNove.value;
+
+    outpromocao.textContent += nove;
+
+}
+
+function  botao5() { 
+    var btCinco = document.getElementById("btCinco");
+    var cinco = btCinco.value;
+    
+    outpromocao.textContent += cinco;
+}	
+
+function  botao6() { 
+    var btSeis = document.getElementById("btSeis");
+    var seis = btSeis.value;
+    
+    outpromocao.textContent += seis;
+}	
+
+function  botao7() { 
+    var btSete = document.getElementById("btSete");
+    var sete = btSete.value;
+    
+    outpromocao.textContent += sete;
+}
+
+function  botao8() {
+    var btOito = document.getElementById("btOito");
+    var oito = btOito.value;
+
+    outpromocao.textContent += oito;
+}
+
+function botao9() {
+    var btNove = document.getElementById("btNove");
+    var nove = btNove.value;
+
+    outpromocao.textContent += nove;
+
+}	
