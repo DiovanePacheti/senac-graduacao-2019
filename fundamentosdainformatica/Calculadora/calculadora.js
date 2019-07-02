079e0d7058f632f1b41a41c63047f9f4a625f7c0
DiovanePacheti <frontendestudo@gmail.com>
2019-07-01 22:51:33 -0300

    criando a funcao que converte binario pra decimal

--- fundamentosdainformatica/Calculadora/calculadora.js
+++ fundamentosdainformatica/Calculadora/calculadora.js
@@ -1,9 +1,9 @@
 var outpromocao = document.getElementById("outpromocao");
-
+var coletor = ""//variavel que vai concatenar a string para enviar as funçoes
 function botao0() {
     var btZero = document.getElementById("btZero");
     var zero = btZero.value;
-
+    coletor += btZero.value;
     outpromocao.textContent += zero;
 
 }
@@ -11,43 +11,105 @@ function botao0() {
 function botao1() {
     var btNove = document.getElementById("btUm");
     var nove = btNove.value;
-
+    coletor += btNove.value;
     outpromocao.textContent += nove;
+
+}
+
+function botao2() {
+    var btDios = document.getElementById("btDois");
+    var dois = btDois.value;
+    coletor += btDois.value;
+    outpromocao.textContent += dois;
+
+}
+
+function botao3() {
+    var btTres = document.getElementById("btTres");
+    var tres = btTres.value;
+    coletor += btTres.value;
+    outpromocao.textContent += tres;
+
+}
+
+
+function botao4() {
+    var btQuatro = document.getElementById("btQuatro");
+    var quatro = btQuatro.value;
+    coletor += btQuatro.value;
+    outpromocao.textContent += quatro;
 
 }
 
 function  botao5() { 
     var btCinco = document.getElementById("btCinco");
     var cinco = btCinco.value;
-    
+    coletor += btCinco.value;
     outpromocao.textContent += cinco;
 }	
 
 function  botao6() { 
     var btSeis = document.getElementById("btSeis");
     var seis = btSeis.value;
-    
+    coletor += btSeis.value;
     outpromocao.textContent += seis;
 }	
 
 function  botao7() { 
     var btSete = document.getElementById("btSete");
     var sete = btSete.value;
-    
+    coletor += btSete.value;
     outpromocao.textContent += sete;
 }
 
 function  botao8() {
     var btOito = document.getElementById("btOito");
     var oito = btOito.value;
-
+    coletor += btOito.value;
     outpromocao.textContent += oito;
 }
 
 function botao9() {
     var btNove = document.getElementById("btNove");
     var nove = btNove.value;
-
+    coletor += btNove.value;
     outpromocao.textContent += nove;
 
 }	
+
+// fim das funções que capturam os valoes dos numeros
+
+// -- Criano a função transformar Numero Binario em Decimal --
+
+function decimal(){
+    
+    //capiturando o valor digitado
+    var valor = coletor;
+    
+    //variavel que ira receber o o resultado em numero decimal
+    var numeroDecimal = 0;
+
+    //variavel que vai inverter o valor de entrada
+    var valorInvertido = "";
+
+    //invertendo valor com loop for
+    for(var i = valor.length-1; i >= 0 ; i--){
+        valorInvertido += valor.charAt(i);
+        }
+    /*vetor de potencia que ira ser somado as potencias que 
+      corresponderem aos valor um da string inputada  
+    */
+    var potencia = [1 , 2 , 4, 8, 16, 32];
+
+    for(var i = valor.length-1; i >= 0; i--){
+            //condicional que ira verificar se deve ser somado o valor
+            if(valorInvertido.charAt(i) == "1"){
+                //somando os valores das potencias
+                numeroDecimal += potencia[i]; 
+            }    
+    }
+    
+    // retornando o resultado 
+    outpromocao.textContent = numeroDecimal + " Decimal ";
+
+}
