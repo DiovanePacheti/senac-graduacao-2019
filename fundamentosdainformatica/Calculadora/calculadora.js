55787ecf60ef37fa4c176ee50ba7104933d214a5
DiovanePacheti <frontendestudo@gmail.com>
2019-07-03 09:09:07 -0300

    criando opercoes decimal e octal

--- fundamentosdainformatica/Calculadora/calculadora.js
+++ fundamentosdainformatica/Calculadora/calculadora.js
@@ -110,7 +110,7 @@ function decimal(){
     }
     
     // retornando o resultado 
-    outpromocao.textContent = numeroDecimal ;
+    outpromocao.textContent = numeroDecimal + " Decimal ";
 
 }//fim da função convertendo binario para decimal
 
@@ -118,10 +118,7 @@ function decimal(){
 function binario(){
     //variavel obtendo valor digitado
     var valor = Number(coletor);
-
-    /*variavel que ira concatenar o resto da 
-      divisao do numero ate zerar
-    */
+    //variavel que ira concatenar o resto da divisao do numero ate zerar
     var numeroBinario = "";
 
     /**
@@ -153,3 +150,83 @@ function binario(){
      outpromocao.textContent = numeroBinarioAjustado;    
     
 }//fim da função binario para 
+
+//função converte octal para decimal
+function octal(){
+    
+    //capiturando o valor digitado
+    var valor = coletor;
+    
+    //variavel que ira receber o o resultado em numero decimal
+    var numeroDecimal = 0;
+    var potencia = [];
+    //variavel que vai inverter o valor de entrada
+    var valorInvertido = "";
+
+    //vetor que ira receber os valores convertidos em 
+    var vetorValorInt = [];
+    //invertendo valor com loop for
+    for(var i = valor.length-1; i >= 0 ; i--){
+        valorInvertido += valor.charAt(i);
+       
+        }
+
+    //vetor que vai receber os valores convertidos em tipo Number int
+    for(var i = 0 ;i < valor.length; i++){
+        vetorValorInt[i] = parseInt(valorInvertido.charAt(i));
+    }   
+    
+    /*vetor de potencia que ira ser somado as potencias que 
+      corresponderem aos valor um da string inputada  
+    */
+    var potencia = [1, 8, 64, 512, 4096, 32768, 262144 ];
+
+    for(var i = 0;i < valor.length; i++){
+            
+                //somando os valores das potencias
+                
+                numeroDecimal += (potencia[i]*vetorValorInt[i]); 
+                
+    }
+    
+    // retornando o resultado 
+    outpromocao.textContent = numeroDecimal;
+
+}//fim da função convertendo binario para decimal
+
+//Criando a função decimal para binario
+function DeOctal(){
+    //variavel obtendo valor digitado
+    var valor = Number(coletor);
+    //variavel que ira concatenar o resto da divisao do numero ate zerar
+    var numeroOctal = "";
+
+    /**
+     * utilizando a estrutura repita um loop
+     * do/while ate o resultado da diviso ser 
+     * zero e terminar a divisao com um break
+     */
+    do{
+        numeroOctal += valor%8
+        divisao = Math.trunc(valor/8);
+        novovalor = divisao
+        valor = novovalor
+        
+
+        if(valor == 0){
+            break;
+        }
+
+    }while(true);       
+    /*variavel que vai inverter a string numeroBinario 
+        para acertar a saida para o sequencia correta
+    */
+    var numeroOctalAjustado ="";
+
+    for(var i = numeroOctal.length; i>=0;i--){
+        
+        numeroOctalAjustado += numeroOctal.charAt(i);
+    }
+     outpromocao.textContent = numeroOctalAjustado;    
+    
+}//fim da função binario para
