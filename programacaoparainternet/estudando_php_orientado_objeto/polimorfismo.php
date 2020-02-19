c7c81435265b5214416f1335b2ff220804c69761
DiovanePacheti <frontendestudo@gmail.com>
2020-02-19 16:59:23 -0300

    alterando logica.html e conteito de polimorfismo

--- programacaoparainternet/estudando_php_orientado_objeto/polimorfismo.php
+++ programacaoparainternet/estudando_php_orientado_objeto/polimorfismo.php
@@ -1,4 +1,32 @@
 <?php
 
+    class Animal{
+        public function andar(){
+            echo "O animal andou !";
+        }//fim do method andar 
+
+        public function correu(){
+            echo " correu !";
+        }
+
+    }//fim da class Animal
+
+    class Cavalo extends Animal{
+
+        /* com o polimorfismo podemos sobrescrever os 
+        metodos herdados da classe mãe para definirmos seu 
+        comportamento na class filho*/
+        public function andar(){
+
+            echo "O Cavalo ";
+            $this->correu();
+        }
+    }
+
+    $animal = new Animal();
+    $animal->Andar();
+    echo "<br>";
+    $novoCavalo = new Cavalo();
+    $novoCavalo->andar();
 
 ?>
