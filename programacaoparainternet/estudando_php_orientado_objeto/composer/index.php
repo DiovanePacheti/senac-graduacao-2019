3e07f3615e601fcf15ef4f7d88ec260a8fcf6b2d
DiovanePacheti <frontendestudo@gmail.com>
2020-02-20 16:49:49 -0300

    iniciando o composer

---
+++ programacaoparainternet/estudando_php_orientado_objeto/composer/index.php
@@ -0,0 +1,16 @@
+<?php /* a pasta vendor ficara os pacotes de terceiros 
+         o arquivo autoload.php  ira carregar todos este pacotes   */
+        
+      require_once 'vendor/autoload.php';
+      //namescape use indica a class que vamos usar
+      use Cocur\Slugify\Slugify;
+
+      $slug =  new Slugify();
+      echo $slug->slugify('teste com acentuação para geração de slug');
+      $slug->addRule('ão', 'erro');
+      echo "<br>".$slug->slugify('teste com acentuação para geração de slug','_');
+
+      
+
+
+?>
